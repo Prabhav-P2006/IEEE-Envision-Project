7f371439b755fc3195e79d0d3f8985b4b0d439f2
Prabhav-P2006 <withprabhav@gmail.com>
2025-03-20 01:28:14 +0530

    Reordered container action URL pattern in urls.py

--- DockerDashboard/containers/urls.py
+++ DockerDashboard/containers/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.container_list, name='container_list'),
-    path('container/<str:container_id>/<str:action>/', views.container_action, name='container_action'),
     path('container/<str:container_id>/logs/', views.container_logs, name='container_logs'),
+    path('container/<str:container_id>/<str:action>/', views.container_action, name='container_action'),
 ]
